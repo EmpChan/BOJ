ba08a36edf321462ea4fc01cb9d54539709572d8
PixelKyrin <hss598876@gmail.com>
2022-12-04 14:19:20 +0900

    [baekjoon] N과 M (4) | 메모리: 2020 KB, 시간: 4 ms

---
+++ baekjoon/N과 M (4).cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[10];
+
+void outit(int s, int e, int r, int c)
+{
+	if (r == c)
+	{
+		for (int i = 0; i < r; i++)
+		{
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+	}
+	else
+	{
+		for (int i = s; i <= e; i++)
+		{
+			arr[c] = i;
+			outit(i, e, r, c + 1);
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, m;
+	cin >> n;
+	cin >> m;
+	outit(1, n, m, 0);
+
+	return 0;
+}
