5f35a289a35795a44d165bac3bfbe8677b93fe27
PixelKyrin <hss598876@gmail.com>
2022-12-18 16:07:47 +0900

    [baekjoon] 킹, 퀸, 룩, 비숍, 나이트, 폰 | 메모리: 2020 KB, 시간: 0 ms

---
+++ baekjoon/킹, 퀸, 룩, 비숍, 나이트, 폰.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int arr[6] = { 1, 1, 2, 2, 2,8};
+	int arr2[6];
+	for (int i = 0; i < 6; i++)
+		cin >> arr2[i];
+	for (int i = 0; i < 6; i++)
+	{
+		cout << arr[i] - arr2[i] << ' ';
+	}
+	return 0;
+}
